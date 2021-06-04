e919b8952ce7ec775e852601c7284fde1fbeeb94
sleepturtle2 <sayantan.mukh98@gmail.com>
2021-06-04 20:11:40 +0530

    added topsort and unweighted DAG header file

---
+++ Graph/unweighted_DAG.h
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+
+class Graph{
+    public: 
+    int V=6; 
+    int E=6; 
+
+    vector<vector<int>>adj = {
+        {}, //0
+        {}, //1
+        {3}, //2
+        {1}, //3
+        {0,1}, //4
+        {2,0} //5
+    }; 
+}; 
