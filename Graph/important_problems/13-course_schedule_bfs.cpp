30e89f83ef6d5016cedce110a86a87b95ffa1b57
Sayantan Mukherjee <sayantan.mukh98@gmail.com>
2021-09-23 20:13:54 +0530

    created bfs solution of course schedule problem
    based on kahn's solution, basically the bfs based solution for the leetcode problem 'course_schedule'

---
+++ Graph/important_problems/13-course_schedule_bfs.cpp
@@ -0,0 +1,38 @@
+//based on kahn's algorithm 
+
+//https://leetcode.com/problems/course-schedule/
+
+bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<vector<int>>adj(n); 
+        
+        int m = pre.size(); 
+        
+        vector<int> in_degree(n, 0); 
+        
+        for(int i=0; i<m; i++)
+        {
+            adj[pre[i][1]].push_back(pre[i][0]); 
+            in_degree[pre[i][0]]++; 
+        }
+        
+        queue<int>q; 
+        for(int i=0; i<n; i++)
+            if(in_degree[i]==0)q.push(i); 
+        
+        int count = 0; 
+        
+        while(!q.empty())
+        {
+            int x = q.front(); 
+            q.pop(); 
+            count++; 
+            
+            for(auto y : adj[x])
+            {
+                if(--in_degree[y] == 0)
+                    q.push(y); 
+            }
+        }
+        //based on kahn's algorithm, that if the number of edges processed is not equal to n, then there must be a cycle. 
+        return n==count ? true : false; 
+    }
